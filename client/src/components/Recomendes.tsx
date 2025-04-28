874802371aee33b655bac4e8d67057cb4f1410b5
bhomik-glitch <awebsite601@gmail.com>
2025-04-28 21:09:21 +0530

    feat: add fullscreen button and single video playback functionality to Recomendes component

--- client/src/components/Recomendes.tsx
+++ client/src/components/Recomendes.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FaExpand } from 'react-icons/fa';
 
 // Import video files
 import ATHARV from "../assets/candidates/ATHARV.mp4";
@@ -36,6 +37,30 @@ const linkedInProfiles = [
 ];
 
 const Recomendes: React.FC = () => {
+  const [currentPlayingVideo, setCurrentPlayingVideo] = useState<number | null>(null);
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+  const handlePlay = (index: number) => {
+    if (currentPlayingVideo !== null && currentPlayingVideo !== index) {
+      // Pause the previously playing video
+      const previousVideo = videoRefs.current[currentPlayingVideo];
+      if (previousVideo) {
+        previousVideo.pause();
+      }
+    }
+    setCurrentPlayingVideo(index);
+  };
+
+  const handleFullscreen = (videoElement: HTMLVideoElement) => {
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+    } else if ((videoElement as any).webkitRequestFullscreen) {
+      (videoElement as any).webkitRequestFullscreen();
+    } else if ((videoElement as any).msRequestFullscreen) {
+      (videoElement as any).msRequestFullscreen();
+    }
+  };
+
   return (
     <>
       {linkedInProfiles.map((item, index) => (
@@ -53,16 +78,27 @@ const Recomendes: React.FC = () => {
             {/* Video section */}
             <div className="relative w-full aspect-video rounded-lg overflow-hidden bg-gray-100">
               <video
+                ref={(el) => {
+                  videoRefs.current[index] = el;
+                }}
                 controls
-                controlsList="nodownload noplaybackrate nofullscreen"
+                controlsList="nodownload noplaybackrate"
                 disablePictureInPicture
-                className="absolute top-0 left-0 w-full h-full object-cover [&::-webkit-media-controls-enclosure]:absolute [&::-webkit-media-controls-enclosure]:bottom-0 [&::-webkit-media-controls-enclosure]:left-0 [&::-webkit-media-controls-enclosure]:right-0 [&::-webkit-media-controls-enclosure]:h-12 [&::-webkit-media-controls-enclosure]:bg-transparent [&::-webkit-media-controls-panel]:bg-gradient-to-t [&::-webkit-media-controls-panel]:from-black/40 [&::-webkit-media-controls-panel]:to-transparent [&::-webkit-media-controls-play-button]:opacity-100 [&::-webkit-media-controls-mute-button]:opacity-100 [&::-webkit-media-controls-timeline]:hidden [&::-webkit-media-controls-current-time-display]:hidden [&::-webkit-media-controls-time-remaining-display]:hidden [&::-webkit-media-controls-volume-slider]:hidden [&::-webkit-media-controls-fullscreen-button]:hidden"
+                className="absolute top-0 left-0 w-full h-full object-cover [&::-webkit-media-controls-enclosure]:absolute [&::-webkit-media-controls-enclosure]:bottom-0 [&::-webkit-media-controls-enclosure]:left-0 [&::-webkit-media-controls-enclosure]:right-0 [&::-webkit-media-controls-enclosure]:h-12 [&::-webkit-media-controls-enclosure]:bg-transparent [&::-webkit-media-controls-panel]:bg-gradient-to-t [&::-webkit-media-controls-panel]:from-black/40 [&::-webkit-media-controls-panel]:to-transparent [&::-webkit-media-controls-play-button]:opacity-100 [&::-webkit-media-controls-mute-button]:opacity-100 [&::-webkit-media-controls-timeline]:hidden [&::-webkit-media-controls-current-time-display]:hidden [&::-webkit-media-controls-time-remaining-display]:hidden [&::-webkit-media-controls-volume-slider]:hidden"
                 title={`${item.name}'s testimonial`}
                 preload="metadata"
+                onPlay={() => handlePlay(index)}
               >
                 <source src={item.video} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
+              <button
+                onClick={() => handleFullscreen(videoRefs.current[index]!)}
+                className="absolute bottom-2 left-2 p-2 bg-black/50 rounded-full text-white hover:bg-black/70 transition-colors duration-200 z-10"
+                title="Toggle fullscreen"
+              >
+                <FaExpand className="w-4 h-4" />
+              </button>
             </div>
           </div>
         </div>
