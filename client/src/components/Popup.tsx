2b56fa910e4b5cf7a636fad806c48fd8bba5cb17
bhomik-glitch <awebsite601@gmail.com>
2025-04-30 14:34:51 +0530

    pop up reduced

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -12,7 +12,7 @@ const Popup = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[500]">
-      <div className="bg-white rounded-lg p-4 max-w-[90%] md:max-w-[700px] lg:max-w-[1250px] relative">
+      <div className="bg-white rounded-lg p-4 max-w-[72%] md:max-w-[560px] lg:max-w-[1000px] relative">
         <button
           onClick={closePopup}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
